3c248495c117d380e2bb6e49f278b7aae13634e0
Robin Bryce <robin.bryce@jitsuin.com>
2020-05-03 17:47:58 +0100

    Initial shape of cluster based on gke-tutorial ref: https://github.com/nkoson/gke-tutorial With the following modifications * All nodes default to preemptable * web-pool -> work-pool * 2 x f1-micro                           ~ £ 4 PCM * 5 x n1-standard-2 (2 vCPU, 7.5 GB Ram) ~ £57 PCM * Default disc 64 GB pd-standard for work-pool * Default disc 10 GB pd-standard for ingress-pool

---
+++ cluster.tf
@@ -0,0 +1,88 @@
+locals {
+  project = " consortia-quorum-preempt"
+}
+
+module "cluster" {
+
+  project                             = local.project
+  source                              = "./gke"
+  region                              = "europe-west2"
+  location                            = "europe-west2-a"
+  cluster_name                        = "kluster"
+  cluster_range_name                  = "gke-pods"
+  services_range_name                 = "gke-services"
+  daily_maintenance_window_start_time = "03:00"
+  subnet_cidr_range                   = "10.0.0.0/16" # 10.0.0.0 -> 10.0.255.255
+  master_ipv4_cidr_block              = "10.1.0.0/28" # 10.1.0.0 -> 10.1.0.15
+  cluster_range_cidr                  = "10.2.0.0/16" # 10.2.0.0 -> 10.2.255.255
+  services_range_cidr                 = "10.3.0.0/16" # 10.3.0.0 -> 10.3.255.255
+  nat_ip_allocate_option              = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat  = "LIST_OF_SUBNETWORKS"
+  source_ip_ranges_to_nat             = ["ALL_IP_RANGES"]
+  nat_log_filter                      = "ERRORS_ONLY"
+  logging_service                     = "none" # $$$
+  monitoring_service                  = "none" # $$$
+
+  node_pools = {
+    ingress-pool = {
+      machine_type       = "f1-micro" # $$$
+      initial_node_count = 1
+      min_node_count     = 1
+      max_node_count     = 2
+      preemptible        = true
+      auto_repair        = true
+      auto_upgrade       = false
+      disk_size_gb       = 10
+      disk_type          = "pd-standard"
+      image_type         = "COS"
+      service_account    = "kluster-serviceaccount@${local.project}.iam.gserviceaccount.com"
+    }
+    work-pool = {
+      machine_type       = "n1-standard-2" # $$$
+      initial_node_count = 3
+      min_node_count     = 1
+      max_node_count     = 5
+      preemptible        = true
+      auto_repair        = true
+      auto_upgrade       = true
+      disk_size_gb       = 64
+      disk_type          = "pd-standard"
+      image_type         = "COS"
+      service_account    = "kluster-serviceaccount@${local.project}.iam.gserviceaccount.com"
+
+    }
+  }
+
+  node_pools_taints = {
+    ingress-pool = [
+      {
+        key    = "ingress-pool"
+        value  = true
+        effect = "NO_EXECUTE"
+      }
+    ]
+    work-pool = []
+  }
+
+  node_pools_tags = {
+    ingress-pool = [
+      "ingress-pool"
+    ]
+    work-pool = [
+      "work-pool"
+    ]
+  }
+
+  node_pools_oauth_scopes = {
+    custom-node-pool = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/service.management",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
