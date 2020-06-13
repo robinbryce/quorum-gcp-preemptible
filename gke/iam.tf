2a1bcce76fc91a99e1113ce1366b86db0bad855e
Robin Bryce <robin.bryce@jitsuin.com>
2020-06-13 17:06:31 +0100

    objectViewer not objectView

--- gke/iam.tf
+++ gke/iam.tf
@@ -56,8 +56,7 @@ resource "google_project_iam_custom_role" "kubeip" {
 
   permissions = [
     "compute.addresses.list",
-    "compute.instances.addAccessConfig",
-    "compute.instances.deleteAccessConfig",
+    "compute.instances.addAccessConfig", "compute.instances.deleteAccessConfig",
     "compute.instances.get",
     "compute.instances.list",
     "compute.projects.get",
@@ -82,4 +81,45 @@ resource "google_project_iam_member" "iam_member_kubeip" {
   project    = var.project
   member     = "serviceAccount:kubeip-serviceaccount@${var.project}.iam.gserviceaccount.com"
   depends_on = [google_service_account.kubeip]
+}
+
+# -----------------------------------------------------------------------------
+# cluster storage bucket iam bindings
+# -----------------------------------------------------------------------------
+resource "google_storage_bucket_iam_binding" "cluster_bucket_project_members_admin" {
+  bucket = google_storage_bucket.cluster.name
+  role = "roles/storage.objectAdmin"
+  members = var.members_bucket_admins
+}
+
+resource "google_storage_bucket_iam_binding" "cluster_bucket_project_members_view" {
+  bucket = google_storage_bucket.cluster.name
+  role = "roles/storage.objectViewer"
+  members = var.members_bucket_admins
+}
+
+# bind the quorum-genesis sa to the policy for the cluster bucket.
+# * https://cloud.google.com/iam/docs/overview
+# * https://www.terraform.io/docs/providers/google/r/storage_bucket_iam.html
+# Note setting the *whole* policy clobbers any existing policy already on the
+# resource - hence google_storage_bucket_iam_policy is a bit of a shotgun and
+# binding is prefered as it is additive.
+resource "google_storage_bucket_iam_binding" "cluster_bucket_quorum_members_admin" {
+  bucket = google_storage_bucket.cluster.name
+  role = "roles/storage.objectAdmin"
+  members = [
+    "${module.quorum-genesis.gcp_service_account_fqn}",
+    "${module.quorum-membership.gcp_service_account_fqn}"
+  ]
+}
+
+resource "google_storage_bucket_iam_binding" "cluster_bucket_quorum_members_view" {
+  bucket = google_storage_bucket.cluster.name
+  role = "roles/storage.objectViewer"
+  members = [
+    "${module.quorum-genesis.gcp_service_account_fqn}",
+    "${module.quorum-membership.gcp_service_account_fqn}",
+    "${module.quorum-node.gcp_service_account_fqn}",
+    "${module.quorum-client.gcp_service_account_fqn}"
+  ]
 }
