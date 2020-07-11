8e6da5531c04035df9f10e869962059d45c32794
Robin Bryce <robin.bryce@jitsuin.com>
2020-07-11 15:24:41 +0100

    tweak the custom iam role

--- cluster/gke/iam.tf
+++ cluster/gke/iam.tf
@@ -54,9 +54,9 @@ resource "google_project_iam_custom_role" "dns01solver" {
   depends_on = [google_project_service.iam]
 
   permissions = [
-    "dns.resourceRecordSets.list",
-    "dns.resourceRecordSets.update",
+    "dns.resourceRecordSets.create",
     "dns.resourceRecordSets.delete",
+    "dns.resourceRecordSets.list",
     "dns.changes.create",
     "dns.changes.get",
     "dns.changes.list",
