fc9837efb3c3723a8cb5f70e0690e5c0b0f9d039
Robin Bryce <robin.bryce@jitsuin.com>
2020-07-15 21:54:44 +0100

    remove (try to) the name spaces from tf

--- cluster/gke/main.tf
+++ cluster/gke/main.tf
@@ -53,31 +53,31 @@ provider "kubernetes" {
 
 # This namespace gets the identity that can resolve dns challenges. This
 # idenity can create and delete dns records
-resource "kubernetes_namespace" "traefik" {
-  metadata {
-    labels = { name = "traefik" }
-    name = "traefik"
-  }
-}
-
-# All the quorum nodes go in here. We don't segregate them further. Any node
-# could potentially do genesis (writing to the storage bucket) and perform raft
-# add/remove. That could be finessed. But this seems enough for a developer
-# oriented setup.
-resource "kubernetes_namespace" "queth" {
-  metadata {
-    labels = { name = "queth" }
-    name = "queth"
-  }
-}
-
-# Can read wallet account keys
-resource "kubernetes_namespace" "caas" {
-  metadata {
-    labels = { name = "caas" }
-    name = "caas"
-  }
-}
+#resource "kubernetes_namespace" "traefik" {
+#  metadata {
+#    labels = { name = "traefik" }
+#    name = "traefik"
+#  }
+#}
+#
+## All the quorum nodes go in here. We don't segregate them further. Any node
+## could potentially do genesis (writing to the storage bucket) and perform raft
+## add/remove. That could be finessed. But this seems enough for a developer
+## oriented setup.
+#resource "kubernetes_namespace" "queth" {
+#  metadata {
+#    labels = { name = "queth" }
+#    name = "queth"
+#  }
+#}
+#
+## Can read wallet account keys
+#resource "kubernetes_namespace" "caas" {
+#  metadata {
+#    labels = { name = "caas" }
+#    name = "caas"
+#  }
+#}
 
 # It seems these modules can *only* be defined in main
 module "workload-identity-kubeip" {
