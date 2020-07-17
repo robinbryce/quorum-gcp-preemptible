850ebdc8956d403bf079a860ea7f1deae3bb8c9f
Robin Bryce <robin.bryce@thalesesecurity.com>
2020-07-17 23:39:52 +0100

    dont depend on things that don't exist, and we don't need the k8s provider

--- cluster/gke/main.tf
+++ cluster/gke/main.tf
@@ -1,8 +1,8 @@
 provider "google" {
   version     = "3.4.0"
-  project = var.project
-  region = var.region
-  zone = var.zone
+  #project = var.project
+  #region = var.region
+  #zone = var.zone
 }
 
 data "google_client_config" "provider" {}
@@ -12,14 +12,14 @@ data "google_container_cluster" "quorumpreempt" {
   project = var.project
 }
 
-provider "kubernetes" {
-  load_config_file = false
-  host = "https://${data.google_container_cluster.quorumpreempt.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.quorumpreempt.master_auth[0].cluster_ca_certificate,
-  )
-}
+#provider "kubernetes" {
+#  load_config_file = false
+#  host = "https://${data.google_container_cluster.quorumpreempt.endpoint}"
+#  token = data.google_client_config.provider.access_token
+#  cluster_ca_certificate = base64decode(
+#    data.google_container_cluster.quorumpreempt.master_auth[0].cluster_ca_certificate,
+#  )
+#}
 
 # This describes all the workload identities and there respective
 # namespaces
@@ -160,7 +160,7 @@ resource "google_container_cluster" "k8s" {
   subnetwork = google_compute_subnetwork.gke-subnet.self_link
 
   workload_identity_config {
-    identity_namespace = "${data.google_container_cluster.quorumpreempt.project}.svc.id.goog"
+    identity_namespace = "${var.project}.svc.id.goog"
   }
   private_cluster_config {
     master_ipv4_cidr_block  = var.master_ipv4_cidr_block
