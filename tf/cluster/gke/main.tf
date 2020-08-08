eb551f0aaa03c301827db0a31740c624f52fe4d9
Robin Bryce <robin.bryce@jitsuin.com>
2020-08-08 09:04:15 +0100

    container registry is just 'there' it doesn't need creating

--- tf/cluster/gke/main.tf
+++ tf/cluster/gke/main.tf
@@ -34,11 +34,6 @@ resource "google_project_service" "container" {
   disable_dependent_services = true
 }
 
-resource "google_container_registry" "container_registry" {
-  project = var.project
-  location = "EU"
-}
-
 resource "google_container_cluster" "k8s" {
   provider           = google-beta
   name               = var.cluster_name
